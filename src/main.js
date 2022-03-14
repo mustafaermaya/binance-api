41c3775c0649657a19eb2c68b0c2b77dcb77b1a9
Mustafa Ermaya <47145354+mustafaermaya@users.noreply.github.com>
2022-03-14 22:07:33 +0300

    axios added to project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { router } from "./router"
 
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://api2.binance.com/"
+});
+
+Vue.prototype.$axios = axiosInstance;
+
 Vue.config.productionTip = false
 
 new Vue({
