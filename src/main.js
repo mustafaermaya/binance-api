f1576eaa8f6dae34067056af1cfca031069a5a4c
Mustafa Ermaya <47145354+mustafaermaya@users.noreply.github.com>
2022-03-21 12:10:18 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { router } from "./router"
 
-import vSelect from 'vue-select'
-import 'vue-select/dist/vue-select.css';
-
-Vue.component('v-select', vSelect)
-
 import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://api2.binance.com/"
